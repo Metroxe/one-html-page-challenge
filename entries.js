078eea4f5d557204713c4c671c9c7226e8c199b8
Christopher Powroznik <chrispow96@gmail.com>
2019-07-13 09:23:21 -0700

    added compatible browser to Ant Colony

--- entries.js
+++ entries.js
@@ -9,7 +9,7 @@
  *     description?: string // description that will be listed with your entry
  *     author?: string // your name/tag that will be listed
  *     github?: string // username on github that will display a link to your profile
- * 		 compatibleBrowsers?: array // browsers that this page is compatible with
+ *     compatibleBrowsers?: array // browsers that this page is compatible with
  * }
  */
 
@@ -27,6 +27,7 @@ const entries = [
 		description: "Simulation of an ant colony creating a never ending underground colony. The 'Q' represents a queen that can giver birth to ants 'A'. Food sources are represented by the numbers 9 - 1. (Currently only works in desktop Chrome)",
 		author: "Christopher Powroznik (Metroxe)",
 		github: "Metroxe",
+		compatibleBrowsers: ["Chrome Desktop"],
 	},
 	{
 		title: "Strange Insults",
@@ -50,20 +51,20 @@ const entries = [
 		github: "huijing",
 		compatibleBrowsers: ["Firefox 66+"],
 	},
-  {
+  	{
 		title: "Pure CSS Still Life - Water and Lemons",
 		filename: "pure-css-still-life-water-lemon.html",
 		description: " A Pure CSS Still Life. No images, No SVG, just CSS, absolutely pointless!",
 		author: "Ben Evans",
 		github: "ivorjetski"
-  },
-  {
-    title: "Calculate Worked Hours",
+  	},
+	{
+	    	title: "Calculate Worked Hours",
 		filename: "calculate_hours_worked.html",
 		description: "Calculates the amount of hours worked based on the start and end time of the work shifts and break times if there is any.",
 		author: "Jacky Ly (lyjacky11)",
 		github: "lyjacky11",
-  },
+	},
 	{
 		title: "Car Game",
 		filename: "mini_car_game.html",
