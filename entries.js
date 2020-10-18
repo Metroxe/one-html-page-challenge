a6656df755c60da83b457558a95263d497e08108
Kritika <52737818+Kritika-Thakkar@users.noreply.github.com>
2020-10-18 19:31:54 +0530

    Added the file

--- entries.js
+++ entries.js
@@ -23,6 +23,13 @@ const entries = [
 		github: "Metroxe",
 	},
 	{
+		title: "Weather site",
+		filename: "weather-site.html",
+		description: "Shows live weather including the highest and lowest.",
+		author: "Kritika Thakkar",
+		github: "Kritika-Thakkar",
+	},
+	{
 		title: "Ant Colony",
 		filename: "ant_colony.html",
 		description: "Simulation of an ant colony creating a never ending underground colony. The 'Q' represents a queen that can giver birth to ants 'A'. Food sources are represented by the numbers 9 - 1. (Currently only works in desktop Chrome)",
