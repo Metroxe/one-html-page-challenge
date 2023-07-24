9bbaa9b850fc4878c67d14e0e38b6947db194f54
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-24 18:56:35 +0100

    stackoverflow.html and added its respective entry in entries.js

--- entries.js
+++ entries.js
@@ -87,6 +87,13 @@ const entries = [
 		author: "Mohamed Mehdi Gara",
 		github: "mohamedmehdigara"
 	},
+	{
+		title: "stackoverflow clone",
+		filename: "stackoverflow.html",
+		description: "Submission to the One Html Page Challenge.",
+		author: "Mohamed Mehdi Gara",
+		github: "mohamedmehdigara"
+	},
 
 	{
 		title: "Car",
