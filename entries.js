eba13a893cf9ff4f3585725a106448bc93845cf2
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-25 12:35:08 +0100

    superman.html and added its respective entry in entries.js

--- entries.js
+++ entries.js
@@ -104,6 +104,14 @@ const entries = [
 	},
 
 	{
+		title: "superman",
+		filename: "superman.html",
+		description: "Submission to the One Html Page Challenge.",
+		author: "Mohamed Mehdi Gara",
+		github: "mohamedmehdigara"
+	},
+
+	{
 		title: "eggs",
 		filename: "eggs.html",
 		description: "Submission to the One Html Page Challenge.",
